3926b34785fa90948840fb5c1a3a2a6e8c8ccd74
fgv02009 <flori.garciavicente@gmail.com>
2021-02-01 21:04:16 -0600

    wip part 4

--- software/apps/ble_service_adv/main.c
+++ software/apps/ble_service_adv/main.c
@@ -12,6 +12,7 @@
 
 #include "nrf52840dk.h"
 
+
 // Intervals for advertising and connections
 static simple_ble_config_t ble_config = {
   // c0:98:e5:4e:xx:xx
@@ -23,19 +24,26 @@ static simple_ble_config_t ble_config = {
   .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS),
 };
 
-static simple_ble_service_t led_service = {{
+//static simple_ble_service_t led_service = {{
+//  .uuid128 = {0x70,0x6C,0x98,0x41,0xCE,0x43,0x14,0xA9,
+//              0xB5,0x4D,0x22,0x2B,0x88,0x10,0xE6,0x32}
+//}};
+//
+//
+//static simple_ble_char_t led_state_char = {.uuid16 = 0x1089};
+//static bool led_state = false;
+//
+static simple_ble_service_t floris_service = {{
   .uuid128 = {0x70,0x6C,0x98,0x41,0xCE,0x43,0x14,0xA9,
               0xB5,0x4D,0x22,0x2B,0x88,0x10,0xE6,0x32}
 }};
 
-static simple_ble_service_t floris_service = {(
-  .uuid128 = {0x70,0x6C,0x98,0x41,0xCE,0x43,0x14,0xA9,
-              0xB5,0x4D,0x22,0x2B,0x88,0x10,0xE6,0x32}
-)}
-
 static simple_ble_char_t led_state_char = {.uuid16 = 0x1089};
 static bool led_state = false;
-
+static simple_ble_char_t notify_state_char = {.uuid16 = 0x108A};
+static bool notify_state = false;
+static simple_ble_char_t button_response_char = {.uuid16 = 0x108B};
+static bool button_pressed_state = false;
 /*******************************************************************************
  *   State for this application
  ******************************************************************************/
@@ -59,7 +67,13 @@ void ble_evt_write(ble_evt_t const* p_ble_evt) {
   }
 }
 
+
 int main(void) {
+
+  nrf_gpio_cfg_input(BUTTON1, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(BUTTON2, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(BUTTON3, NRF_GPIO_PIN_PULLUP);
+  nrf_gpio_cfg_input(BUTTON4, NRF_GPIO_PIN_PULLUP);
 
   printf("Board started. Initializing BLE: \n");
 
@@ -69,16 +83,29 @@ int main(void) {
   // Setup BLE
   simple_ble_app = simple_ble_init(&ble_config);
 
-  simple_ble_add_service(&led_service);
+  simple_ble_add_service(&floris_service);
   simple_ble_add_characteristic(1, 1, 0, 0,
       sizeof(led_state), (uint8_t*)&led_state,
-      &led_service, &led_state_char);
+      &floris_service, &led_state_char);
+  simple_ble_add_characteristic(1,0,1,0,
+      sizeof(notify_state), (uint8_t*)&notify_state,
+      &floris_service, &notify_state_char); //read and notify but dont write
 
   // Start Advertising
   simple_ble_adv_only_name();
 
+
   while(1) {
-    power_manage();
+    if((!nrf_gpio_pin_read(BUTTON1)) || (!nrf_gpio_pin_read(BUTTON2)) || (!nrf_gpio_pin_read(BUTTON3)) || (!nrf_gpio_pin_read(BUTTON4))){
+      printf("pushed button sensed");
+      if(notify_state){
+        notify_state = false;
+      } else {
+        notify_state = true;
+      }
+        simple_ble_notify_char(&notify_state_char);
+    }
+    //power_manage();
   }
 }
 
